7b603f497d760fd81e9e8b1d6d00e1c19d1f1efc
pandaman64 <kointosudesuyo@infoseek.jp>
2023-07-22 14:31:12 +0900

    Add link to individual page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import {
   type LoaderArgs,
   type V2_MetaFunction,
 } from "@remix-run/cloudflare";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 import { eq } from "drizzle-orm";
 import { getAuthenticator } from "~/auth.server";
 import { Button } from "~/components/ui/button";
@@ -64,14 +64,16 @@ function LogoutForm() {
 
 function ArticleCard(article: Article) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-lg">
-          {article.title.length > 0 ? article.title : article.content}
-        </CardTitle>
-        <CardDescription>{article.content}</CardDescription>
-      </CardHeader>
-    </Card>
+    <Link to={`/articles/${article.id}`}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-lg">
+            {article.title.length > 0 ? article.title : article.content}
+          </CardTitle>
+          <CardDescription>{article.content}</CardDescription>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 }
 
--- app/routes/auth.logout.tsx
+++ app/routes/auth.logout.tsx
@@ -1,10 +1,5 @@
-import type { LoaderArgs, ActionArgs } from "@remix-run/cloudflare";
+import type { ActionArgs } from "@remix-run/cloudflare";
 import { getAuthenticator } from "~/auth.server";
-
-export async function loader({ request, context }: LoaderArgs) {
-  const authenticator = getAuthenticator(context);
-  await authenticator.logout(request, { redirectTo: "/" });
-}
 
 export async function action({ request, context }: ActionArgs) {
   const authenticator = getAuthenticator(context);
